038d8fd365cc5d325e104035738d7ce871f42ae1
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-06-29 21:50:49 +0700

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '../UserProvider';
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "../UserProvider";
+import { NhostNextProvider, NhostClient } from "@nhost/nextjs";
+
+const nhost = new NhostClient({
+   backendUrl: process.env.NEXT_PUBLIC_NHOST_BACKEND_URL || "",
+});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
-  );
+   return (
+      <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+         <UserProvider>
+            <Component {...pageProps} />
+            <Toaster />
+         </UserProvider>
+      </NhostNextProvider>
+   );
 }
-
-export default MyApp;
